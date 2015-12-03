9499849f42a9030a345cf48d5c8be9ceb3033130
Kory Kraft <kraftko@onid.oregonstate.edu>
2015-12-02 22:53:09 -0800

    Short term circle detection OKish when teleoperating. Still picks up edges. It would be nice to only pick out convex circles from perspective. To do long term circles, need to do tf from circle to tf.

--- src/circle_detector.py
+++ src/circle_detector.py
@@ -4,6 +4,7 @@
 '''
 
 import rospy
+import tf
 from std_msgs.msg import *
 from geometry_msgs.msg import Pose, Quaternion, Point, Vector3
 from sensor_msgs.msg import LaserScan
@@ -11,6 +12,8 @@ from visualization_msgs.msg import Marker, MarkerArray
 
 from math import sin,cos,atan2,pi,sqrt
 import matplotlib.pyplot as plt
+
+import random
 
 ## for ellipse fitting
 import numpy as np
@@ -27,34 +30,65 @@ class CircleDetector:
         self.last_a = None
         self.last_b = None
         self.last_center = None
-        # self.red = ColorRGBA(1, 0, 0, 1)
-        # self.green = ColorRGBA(0, 1, 0, 1)
+        self.red = ColorRGBA(1, 0, 0, 1)
+        self.green = ColorRGBA(0, 1, 0, 1)
         # self.color = self.red
+
+        self.tf_broadcaster = tf.TransformBroadcaster() # broadcasts the tf from the found ellipse to the scanner
+        self.tf_listener = tf.TransformListener() # used to get the tf from the map to the ellipse
+
 
         self.scan_sub = rospy.Subscriber(laser_topic, LaserScan, self.laser_cb)
         self.marker_array_pub = rospy.Publisher("circle_marker_array", MarkerArray, queue_size=10)
 
-        print "Instantiated circle detector"
+        self.suspected_circle_centers = []
+        self.laser_counter = 0
+        self.laser_update_size = 20
+
+        print "Circle Detector Started"
 
 
 
     def laser_cb(self, data):
-        # Fit ellipse
-        # If ellipses, create and place markers
-
         # self.fit_ellipse(data) # single ellipse expected
         ellipses = self.fit_multi_ellipse(data) # multiple ellipses expected
 
         # create markers
         markers = []
+
+        # create initial markers for the suspected circles
         if ellipses is not None:
-            for ellipse in ellipses:
+            for i in xrange(len(ellipses)):
+                ellipse = ellipses[i]
                 print "Found ellipse"
-                marker = self.create_circle_marker(ellipse.center[0], ellipse.center[1], ellipse.theta, ellipse.a, ellipse.b)
+
+                # add init marker for the circle
+                marker = self.create_circle_marker(ellipse.center[0], ellipse.center[1], ellipse.theta, ellipse.a, ellipse.b, marker_id=i, color=self.green)
                 markers.append(marker)
+
+                ## add ellipse to possible list
+                self.suspected_circle_centers.append((ellipse.center[0], ellipse.center[1]))
+
+
+        ### Create markers for "confirmed" circles (those confirmed by clustering algorithm)
+        ### keep track of when to cull ellipses/circles by doing a 
+        #### clustering algorithm on the cirlces from time to time
+        # self.laser_counter += 1
+        # if self.laser_counter > self.laser_update_size:
+        #     self.laser_counter = 0
+
+        #     ## cluster centers
+        #     clustered_circles = self.cluster_circle_centers()
+
+        #     if clustered_circles is not None:
+        #         for center in clustered_circles:
+        #             # add confirmed marker to array
+        #             marker = self.create_circle_marker(center[0], center[1], 0.0, 0.5, 0.5, marker_id=random.randint(101,200), color=self.red)
+        #             markers.append(marker)
 
         # place markers
         self.marker_array_pub.publish(markers)
+
 
 
     # Fit an ellipse to the data
@@ -131,8 +165,8 @@ class CircleDetector:
             try:
                 e.fit(xy)
                 print e
-                if self.is_valid_ellipse(e):
-                    print "  valid ellipse"
+                if self.is_valid_circle(e):
+                    # print "  valid ellipse"
                     new_ellipses.append(e)
             except:
                 pass
@@ -141,7 +175,7 @@ class CircleDetector:
         return new_ellipses
             
         
-    def is_valid_ellipse(self, ellipse):
+    def is_valid_circle(self, ellipse):
         # Validity is measured by it being a real ellipse, with 
         #   values in the plausible range for representing a human
         # if (ellipse.is_valid() and 
@@ -152,31 +186,75 @@ class CircleDetector:
         #     return False
 
         if ellipse.is_valid():
-            return True
+            diff = abs(ellipse.a - ellipse.b)
+            # print "diff = ", diff
+            if diff < 0.1 and ellipse.a < 1.2:
+                return True
+            else:
+                return False
         else:
             return False
 
 
-    def create_circle_marker(self, pose_x, pose_y, ellipse_theta, ellipse_a, ellipse_b):
+    def create_circle_marker(self, pose_x, pose_y, ellipse_theta, ellipse_a, ellipse_b, marker_id=0, color=ColorRGBA(0, 1, 0, 1)):
         h = Header()
         # h.frame_id = self.scan_frame_id #tie marker visualization to laser it comes from
-        h.frame_id = "map"
+        h.frame_id = "base_laser_link"
         h.stamp = rospy.Time.now() # Note you need to call rospy.init_node() before this will work
         
         #create marker:person_marker, modify a red cylinder, last indefinitely
         mark = Marker()
         mark.header = h
         mark.ns = "ellipse_marker"
-        mark.id = 0
+        mark.id = marker_id
         mark.type = 3
         mark.action = 0
         mark.scale = Vector3(ellipse_a*2, ellipse_b*2, 1) #scale, in meters
-        mark.color = ColorRGBA(0, 1, 0, 1) 
+        mark.color = color
 
         pose = Pose(Point(pose_x, pose_y, 0.5), Quaternion(0.0,0.0,1.0,cos(ellipse_theta/2)))
         mark.pose = pose
 
         return mark
+
+    def cluster_circle_centers(self):
+        
+        print "\n\n\nClustering centers"
+        cluster_centers = []
+
+        centers_array = np.asarray(self.suspected_circle_centers)
+
+        if len(centers_array) > 3:
+            db = DBSCAN(eps=0.5, min_samples=3).fit(centers_array)
+            print db
+        else:
+            return None
+
+        labels = db.labels_
+        
+        # Number of clusters in labels, ignoring noise if present.
+        n_clusters = len(set(labels)) - (1 if -1 in db.labels_ else 0)
+        print "Num clusters/centers = ", n_clusters
+
+
+        # compute centroid of cluster
+        for n in xrange(n_clusters):
+            print "label for cluster: ", n
+
+            # comput centroid with averaging technique
+            locs_in_cluster = centers_array[labels==n]
+            x_avg = np.mean(locs_in_cluster[:,0])
+            y_avg = np.mean(locs_in_cluster[:,1])
+
+            cluster_center = (x_avg, y_avg)
+            print "Found cluster around: ", cluster_center
+            
+            # unique_labels = set(labels)
+            cluster_centers.append(cluster_center)
+
+        print "Clusters found in total: ", cluster_centers
+        return cluster_centers
+
 
 
 if __name__ == '__main__':
